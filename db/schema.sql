cf4ec9e476627159688762182a88b4e9efd4a6e2
Stevo <steven.barrios92@gmail.com>
2022-09-28 19:09:31 -0700

    no good

--- db/schema.sql
+++ db/schema.sql
@@ -18,15 +18,14 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL
 )
